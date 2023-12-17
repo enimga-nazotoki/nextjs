7fa890676057cc318f3dd6c0899ab5915bdd3898
enigma-nazotoki <johndoe@email.com>
2023-12-17 19:41:27 +0900

    commit test

---
+++ src/pages/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => {
+    return <header>
+        <div className="logo">
+            <h3>大阪公立大学謎解きイベント制作団体 F-enigma</h3>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="http://localhost:3000/">Home</a>
+                </li>
+                <li>
+                    <a href="about/">About</a>
+                </li>
+                <li>
+                    <a href="#">Contents</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+};
+
+export default Header;
