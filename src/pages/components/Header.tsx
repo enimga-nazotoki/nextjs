fcf3b65066e29aee2c06adf79af098733c9d725c
murata <tomoya49m@gmail.com>
2024-04-04 18:36:33 +0900

    0404

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -41,58 +41,59 @@ const Header = () => {
             <a href="https://twitter.com/omunazotoki" target="_blank">
             <Image src="/logo-white.png" alt="X" width={40} height={40} style={{ width: '80%', height: 'auto' }}/></a>
         </div>
-        <div className={styles.for_sp}>
-            <nav className={
+      <nav
+        className={
+          isOpen
+            ? "z-40 bg-blue-100 fixed top-0 right-0 bottom-0 left-0 h-screen flex flex-col"
+            : "fixed right-[-100%] md:right-4"
+        }
+      >
+        <ul
+          className={
             isOpen
-                ? "z-40 bg-blue-100 fixed top-0 right-0 bottom-0 left-0 h-screen flex flex-col"
-                : "fixed right-[-100%] md:right-4"
-            }
-            >
-            <ul className={
-                isOpen
-                ? "flex h-screen justify-center items-center flex-col gap-6 text-xl"
-                : "block md:flex md:gap-8"
-            }
-            >
-            <li>
-                <Link onClick={handleMenuClose} href="/about">
-                About
-                </Link>
-            </li>
-            <li>
-                <Link onClick={handleMenuClose} href="/company">
-                Company
-                </Link>
-            </li>
-            <li>
-                <Link onClick={handleMenuClose} href="/recruit">
-                Recruit
-                </Link>
-            </li>
-            </ul>
-            </nav>
-            <button className="z-50 space-y-2 md:hidden" onClick={handleMenuOpen}>
-            <span
-            className={
-                isOpen
-                ? "block w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 duration-300"
-                : "block w-8 h-0.5 bg-gray-600 duration-300"
-            }
-            />
-            <span
-            className={
-                isOpen ? "block opacity-0 duration-300" : "block w-8 h-0.5 bg-gray-600 duration-300"
-            }
-            />
-            <span
-            className={
-                isOpen
-                ? "block w-8 h-0.5 bg-gray-600 -rotate-45 duration-300"
-                : "block w-8 h-0.5 bg-gray-600 duration-300"
-            }
-            />
-            </button>
-        </div>
+              ? "flex h-screen justify-center items-center flex-col gap-6 text-xl"
+              : "block md:flex md:gap-8"
+          }
+        >
+          <li className={styles.for_sp}>
+            <Link onClick={handleMenuClose} href="/about">
+              About
+            </Link>
+          </li>
+          <li className={styles.for_sp}>
+            <Link onClick={handleMenuClose} href="/contents">
+              Contents
+            </Link>
+          </li>
+          <li className={styles.for_sp}>
+            <Link onClick={handleMenuClose} href="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <button className={"z-50 space-y-2 md:hidden"} onClick={handleMenuOpen}>
+        <span
+          className={
+            /* ×についての記述 */
+            isOpen
+              ? "block w-8 h-0.5 mr-4 bg-gray-600 translate-y-2.5 rotate-45 duration-300"
+              : "block w-8 h-0.5 mr-4 bg-gray-600 duration-300"
+          }
+        />
+        <span
+          className={
+            isOpen ? "block opacity-0 duration-300" : "block w-8 h-0.5 bg-gray-600 duration-300"
+          }
+        />
+        <span
+          className={
+            isOpen
+              ? "block w-8 h-0.5 bg-gray-600 -rotate-45 duration-300"
+              : "block w-8 h-0.5 bg-gray-600 duration-300"
+          }
+        />
+      </button>
     </header>
 };
 
