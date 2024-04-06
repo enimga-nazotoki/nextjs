c2111f79462b4aa9cea6d7b1053d20e7cdb1b669
murata <tomoya49m@gmail.com>
2024-04-06 17:58:59 +0900

    0406

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, {useRef} from 'react'
 import styles from '@/styles/Home.module.css'
+import Router from 'next/router'
 
 
 export default function Contact() {
@@ -25,38 +26,48 @@ export default function Contact() {
             },
             body: JSON.stringify(data),
         }).then((res) => {
-            if (res.status === 200) console.log("メール送信成功");
+            if (res.status === 200) Router.push('success/');
         });
     };
 
     return (
         <>
         <main className={`${styles.main}`}>
-        <div className='container mt-5'>
-            <h2 className='mb-3'>入部希望者募集</h2>
-            <h4 className='mb-10'>
-                F-enigmaでは随時新入部員を募集しています。大阪府立大学・大阪市立大学・大阪公立大学の学生であれば、どなたでも入部できます。もちろんどのキャンパスでもOK！お気軽にご連絡ください！
-            </h4>
-            <h2 className='mb-3'>制作依頼募集</h2>
-            <h4 className='mb-10'>
-                F-enigmaでは様々な形式・媒体での謎解き制作依頼を受け付けています。具体的な内容や納期、料金についてご相談させていただきたいので、まずはご連絡ください。
-            </h4>            
+        <div className='container mt-3'>
+            <div className="p-3 mb-2 bg-white text-dark">
+                <h2 className='mb-3'>入部希望者募集</h2>
+                <h4>
+                    F-enigmaでは随時新入部員を募集しています。大阪府立大学・大阪市立大学・大阪公立大学の学生であれば、どなたでも入部できます。もちろんどのキャンパスでもOK！お気軽にご連絡ください！
+                </h4>
+            </div>
+            <div className="p-3 mb-10 bg-white text-dark">
+                <h2 className='mb-3'>制作依頼募集</h2>
+                <h4>
+                    F-enigmaでは様々な形式・媒体での謎解き制作依頼を受け付けています。具体的な内容や納期、料金についてご相談させていただきたいので、まずはご連絡ください。
+                </h4>         
+            </div>        
             <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
                 <div className='mb-3'>
                     <label htmlFor='name' className='form-label'>
+                        <div className="p-0 mb-2 bg-white text-dark">
                         お名前
+                        </div>
                     </label>
                     <input type='text' className='form-control' id='name' required ref={nameRef}/>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor='email' className='form-label'>
+                        <div className="p-0 mb-2 bg-white text-dark">
                         メールアドレス
+                        </div>
                     </label>
                     <input type='text' className='form-control' id='email' required ref={emailRef}/>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor='message' className='form-label'>
+                        <div className="p-0 mb-2 bg-white text-dark">
                         メッセージ
+                        </div>
                     </label>
                     <textarea name='message' className='form-control' id='message' ref={messageRef}/>
                 </div>
