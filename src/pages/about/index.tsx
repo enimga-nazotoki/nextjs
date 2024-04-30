ae931ac666103f6b868e6d05423a144e19c6d7a8
murata <tomoya49m@gmail.com>
2024-04-30 21:58:16 +0900

    0430 murata

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -31,6 +31,7 @@ export default function About() {
             <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet"></link>
             </Head>
             <main className={`${styles.main} ${inter.className}`}>
+              <div className='container mt-3'>
               <div style={LikeB}>
                 <h2>〇活動内容</h2>
               </div>
@@ -87,7 +88,7 @@ export default function About() {
                 </h5> 
               
               </div>
-
+              </div>
 
             </main>
         </>
